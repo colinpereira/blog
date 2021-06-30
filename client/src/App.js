fd5130cfe3a4cd157fb8f6330384e33266e93c27
colinpereira <64594861+colinpereira@users.noreply.github.com>
2021-06-30 15:52:39 -0700

    Initialized client, server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+//Importing components
+import Home from "./Pages/Home";
+import CreatePost from "./Pages/CreatePost";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact render={(props) => <Home />} />
+        <Route
+          path="/CreatePost/:id"
+          exact
+          render={(props) => <CreatePost />}
+        />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
