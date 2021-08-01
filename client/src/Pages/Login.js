bcd4c74101c48133220543b2ce9099b651b06eda
colinpereira <64594861+colinpereira@users.noreply.github.com>
2021-08-01 22:16:03 +0400

    login ui

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./Styles/Login.css";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
+  return (
+    <div className="loginContainer">
+      <div className="login">
+        <h1>Login</h1>
+        <label>Username:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <label>Password:</label>
+        <input
+          autoComplete="off"
+          type="password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+
+        <button> Login </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
