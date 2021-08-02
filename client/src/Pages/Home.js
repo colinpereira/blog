1ebb4824050d9f55b829ad230cb50a4481cc19b8
colinpereira <64594861+colinpereira@users.noreply.github.com>
2021-08-02 17:36:17 +0400

    liking ui

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -3,11 +3,13 @@ import Axios from "axios";
 import "./Styles/Home.css";
 import Bin from "./Styles/Images/bin.png";
 import Modal from "../Components/Modal";
+import Like from "./Styles/Images/like.svg";
 
 function Home() {
   const [postList, setPostList] = useState([]);
   const [openModal, setOpenModal] = useState(false);
   const [currValues, setCurrValues] = useState({});
+  const [likes, setLikes] = useState(0);
 
   useEffect(() => {
     Axios.get("http://localhost:3001/posts/api/get").then((response) => {
@@ -28,13 +30,17 @@ function Home() {
     );
   };
 
+  const incrementLike = () => {
+    setLikes(likes + 1);
+  };
+
   return (
     <div className="home">
       <header className="homeHeader">Welcome to my blog!</header>
 
       <div className="postContainer">
         {postList &&
-          postList.map((post, key) => {
+          postList.map((post, key = key) => {
             return (
               <div className="post">
                 <h1 className="postTitle">{post.title}</h1>
@@ -43,21 +49,30 @@ function Home() {
                 </h3>
                 <p className="postDescription">{post.description}</p>
                 <div className="postActionContainer">
-                  <p
-                    onClick={() => {
-                      setOpenModal(true);
-                      setCurrValues(post);
-                      console.log(openModal);
-                    }}
-                  >
-                    Update Post
-                  </p>
-                  <img
-                    src={Bin}
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  />
+                  <div className="likeIcon">
+                    <img src={Like} onClick={incrementLike} />
+                    <div className="likeAmt">
+                      {likes > 0 && <h3>{likes}</h3>}
+                    </div>
+                  </div>
+                  <div className="postActionContainer-right">
+                    <p
+                      onClick={() => {
+                        setOpenModal(true);
+                        setCurrValues(post);
+                        console.log(openModal);
+                      }}
+                    >
+                      Update Post
+                    </p>
+                    <img
+                      src={Bin}
+                      className="binIcon"
+                      onClick={() => {
+                        deletePost(post.id);
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
             );
